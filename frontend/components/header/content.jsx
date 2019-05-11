b655a477b7d36b4bd2348f320ef82ca9735a8982
steven <stvtortora@gmail.com>
2019-05-11 18:27:56 -0400

    add front page content styling

--- frontend/components/header/content.jsx
+++ frontend/components/header/content.jsx
@@ -1,10 +1,42 @@
 import React from 'react'
-import HomePageContent from './HomePageContent'
+
 
 export default () => {
+  const sectionHeader =  () => {
+    return (
+      <div className='home-section-header'>
+        <h3>The Easiest Way To Get Your New Job</h3>
+        <span>Find Full Time, Part Time, or Freelance Opportunities</span>
+      </div>
+    )
+  }
+
+  const searchBox = () => {
+    return (
+      <section className='home-search-box'>
+        <form>
+          <div className='search-keywords'>
+            <input id='keyword-input' placeholder='Job title, keyword, or company name'></input>
+            <i class="fa fa-keyboard-o"></i>
+          </div>
+          <div className='search-location'>
+            <input id='location-input' placeholder='Choose your city'></input>
+            <i class="fa fa-map-marker" aria-hidden="true"></i>
+          </div>
+          <div className='search-submit'>
+            <button><i class="fa fa-search" aria-hidden="true"></i></button>
+          </div>
+        </form>
+      </section>
+    )
+  }
+
   return (
     <content className='content'>
-      <HomePageContent/>
+      <div className='home-page-content'>
+          {sectionHeader()}
+          {searchBox()}
+        </div>
     </content>
   )
 }
--- frontend/components/header/homePageContent.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default () => {
-  return (
-    <div className='home-page-content'>
-      <div>Hello</div>
-    </div>
-  )
-}
