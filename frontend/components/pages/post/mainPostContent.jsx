095e2f339b81d0b87a3c50669303ed65f83fd9bc
steven <stvtortora@gmail.com>
2019-05-18 21:02:18 -0400

    style post header

--- frontend/components/pages/post/mainPostContent.jsx
+++ frontend/components/pages/post/mainPostContent.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 
 export default ({ post, postType, additionalInfo }) => {
-  const descriptionBox = () => {
+  const description = () => {
     return (
       <section className='post-description-container'>
         <p className='post-description-title'>{`${postType} Description`}</p>
@@ -13,14 +13,14 @@ export default ({ post, postType, additionalInfo }) => {
   const header = () => {
     return (
       <header className='post-header'>
-        <p className='post-header-title'>{post.title}</p>
+        <h3 className='post-header-title'>{post.title}</h3>
         <div className='header-second-line'>
-          <p>{post.city}</p>
+          <p><i className="fa fa-map-marker" aria-hidden="true"></i> {post.city}</p>
           <p>{additionalInfo}</p>
         </div>
         <div className='header-links'>
-        <a className='header-link' href={post.website}>{post.website}</a>
-          <a className='header-link' href={post.linked_in}>{post.linked_in}</a>
+          <a className='header-link' href={post.website}><i class="fa fa-link" aria-hidden="true"></i> {post.website}</a>
+          <a className='header-link' href={post.linked_in}><i class="fa fa-linkedin-square" aria-hidden="true"></i> {post.linked_in}</a>
         </div>
       </header>
     )
@@ -29,7 +29,7 @@ export default ({ post, postType, additionalInfo }) => {
   return (
     <div className='main-post-content'>
       {header()}
-      {descriptionBox()}
+      {description()}
     </div>
   )
 }
